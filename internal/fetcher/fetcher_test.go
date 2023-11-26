2433620fb9b74030fb38923de3c3294a34d8f676
Pensk <superpensk@gmail.com>
2023-11-26 10:47:17 +0900

    Add a test for the FetchAndSave function

---
+++ internal/fetcher/fetcher_test.go
@@ -0,0 +1,43 @@
+package fetcher
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func TestFetchAndSave(t *testing.T) {
+	html := "<html><body>OK</body></html>"
+
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.Write([]byte(html))
+	}))
+	serverURL := server.URL + "/OK"
+
+	defer server.Close()
+
+	err := FetchAndSave(serverURL)
+	if err != nil {
+		t.Errorf("FetchAndSave failed: %v", err)
+	}
+
+	filename := "OK.html"
+
+	// Check the file was created
+	_, err = os.Stat(filename)
+	if os.IsNotExist(err) {
+		t.Errorf("File was not created")
+	}
+
+	// Check the contents of the file
+	contents, err := os.ReadFile("OK.html")
+	if err != nil {
+		t.Errorf("Failed to read file: %v", err)
+	}
+	if string(contents) != html {
+		t.Errorf("File contents did not match server output")
+	}
+
+	os.Remove("OK.html")
+}
